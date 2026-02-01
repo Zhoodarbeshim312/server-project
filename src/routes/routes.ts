a3274a55dbf7a2f42562ebc1e575644bdb91de6c
Zhoodarbeshim <joodarbeshimaktanov@gmail.com>
2026-02-02 00:26:18 +0600

    updated localhost

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -5,7 +5,7 @@ import crudRouter from "../modules/crud/crud.routes";
 import userRouter from "../modules/user/user.routes";
 const globalRouter = Router();
 const corsConfig = {
-  origin: ["http://localhost:3000", "http://localhost:5173/"],
+  origin: ["http://localhost:3000", "http://localhost:5173"],
 };
 globalRouter.use("/auth", cors(corsConfig), authRouter);
 globalRouter.use("/crud", cors(corsConfig), crudRouter);
