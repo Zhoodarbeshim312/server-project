1b86e9dbe47a2721d88fc1fa749707dd0efd9936
Zhoodarbeshim <joodarbeshimaktanov@gmail.com>
2026-02-02 00:22:45 +0600

    building

--- dist/routes/routes.js
+++ dist/routes/routes.js
@@ -10,7 +10,7 @@ const crud_routes_1 = __importDefault(require("../modules/crud/crud.routes"));
 const user_routes_1 = __importDefault(require("../modules/user/user.routes"));
 const globalRouter = (0, express_1.Router)();
 const corsConfig = {
-    origin: ["http://localhost:3000"],
+    origin: ["http://localhost:3000", "http://localhost:5173/"],
 };
 globalRouter.use("/auth", (0, cors_1.default)(corsConfig), auth_routes_1.default);
 globalRouter.use("/crud", (0, cors_1.default)(corsConfig), crud_routes_1.default);
